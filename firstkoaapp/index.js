8d7875cb0953be75684a1364be63785346384156
Jack Lombardo <60198678+JackLombardo@users.noreply.github.com>
2022-02-23 22:03:39 -0500

    init commit

---
+++ firstkoaapp/index.js
@@ -0,0 +1,20 @@
+//create our server object
+const koa = require('koa');
+let server = new koa();
+
+//create static folder - frontend
+const static = require('koa-static');
+
+//creating our router
+const Router = require('koa-router');
+const route = require('koa-router');
+
+//routes
+route.get('/',(ctx, next) => ctx.body = 'Hello World');
+
+//middleware - between request and response
+server.use(route.routes())
+server.use(static('./public'));
+
+//our listener on port 1985
+server.listen(1985, 'localhost',() => console.log('Listening on port 1985'));
